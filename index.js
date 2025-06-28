f99ec70e84a202f0efec295f13738d88614a58e8
kg07nurislam <152377115+kg07nurislam@users.noreply.github.com>
2025-06-28 08:49:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const port = process.env.PORT || 4000
 
 bot.start((ctx) => ctx.reply('Салам! Мен kg07nurislam`дын ботумун.'));
 bot.help((ctx) => ctx.reply('Бул бот 24/7 иштейт.'));
