2123d842e0ced27862eb640fa956f139402816ea
kg07nurislam <152377115+kg07nurislam@users.noreply.github.com>
2025-06-28 08:16:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const express = require('express');
 const { Telegraf } = require('telegraf');
 
-// BOT_TOKEN .env файлдан алат
+const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Салам! Мен Renderде иштеп жатам.'));
+bot.start((ctx) => ctx.reply('Салам! Мен kg07nurislam`дын ботумун.'));
 bot.help((ctx) => ctx.reply('Бул бот 24/7 иштейт.'));
 
 bot.launch();
+
+app.get('/', (req, res) => {
+  res.send('Бот жандуу!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
